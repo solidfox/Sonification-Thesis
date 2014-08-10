655afe08a89ffe88b07f184d3d06a26333f36e4c
Daniel Schlaug <daniel.schlaug@gmail.com>
2014-08-10 10:31:04 +0200

    Added code of the implementation

---
+++ Sonji/Sonji/CharacterCanvas.swift
@@ -0,0 +1,34 @@
+//
+//  CharacterCanvas.swift
+//  Sonji
+//
+//  Created by Daniel Schlaug on 7/5/14.
+//  Copyright (c) 2014 Daniel Schlaug. All rights reserved.
+//
+
+import UIKit
+
+class CharacterCanvas: PointSequenceView {
+    func animateFailure() {
+        self.clear()
+    }
+    
+    func animateSuccess() {
+        self.clear()
+    }
+    
+    func _defaultSettings() {
+        self.opaque = false
+        self.interpolatePoints = true
+    }
+    
+    init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+        self._defaultSettings()
+    }
+    
+    init(frame: CGRect) {
+        super.init(frame:frame)
+        self._defaultSettings()
+    }
+}
